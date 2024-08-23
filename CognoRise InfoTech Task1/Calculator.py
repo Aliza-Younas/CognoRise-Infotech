c90011e703a305d98a337080838828fcf63b8c5d
Aliza Younas <alizayounas488@gmail.com>
2024-08-23 13:13:21 -0700

    Add files via upload

---
+++ CognoRise InfoTech Task1/Calculator.py
@@ -0,0 +1,50 @@
+def add(P, Q):
+    # This function is used for adding two numbers   
+    return P + Q
+
+def subtract(P, Q):
+    # This function is used for subtracting two numbers   
+    return P - Q
+
+def multiply(P, Q):
+    # This function is used for multiplying two numbers   
+    return P * Q
+
+def divide(P, Q):
+    # This function is used for dividing two numbers    
+    return P / Q
+
+while True:
+    # Display the options
+    print("Please select the operation.")
+    print("a. Add")
+    print("b. Subtract")
+    print("c. Multiply")
+    print("d. Divide")
+
+    # Take the user's choice
+    choice = input("Please enter choice (a/ b/ c/ d): ")
+
+    # Take the input numbers
+    num_1 = int(input("Please enter the first number: "))
+    num_2 = int(input("Please enter the second number: "))
+
+    # Perform the chosen operation
+    if choice == 'a':
+        print(num_1, " + ", num_2, " = ", add(num_1, num_2))
+    elif choice == 'b':
+        print(num_1, " - ", num_2, " = ", subtract(num_1, num_2))
+    elif choice == 'c':
+        print(num_1, " * ", num_2, " = ", multiply(num_1, num_2))
+    elif choice == 'd':
+        print(num_1, " / ", num_2, " = ", divide(num_1, num_2))
+    else:
+        print("This is an invalid input")
+
+    # Ask the user if they want to continue
+    again = input("Do you want to perform another calculation? (yes/no): ").lower()
+
+    # Exit the loop if the user does not want to continue
+    if again != 'yes':
+        print("Goodbye!")
+        break
